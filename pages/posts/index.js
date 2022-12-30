66f26497a959acacd30147a8684480a58f4c4de7
Miha Stele <stele1000@gmail.com>
2022-12-30 15:21:52 +0100

    Add dummy all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,31 @@
 import Link from "next/link"
 import Logo from "../../components/layout/logo"
+import AllPosts from "../../components/posts/all-posts"
+
+const DUMMY_POSTS = [{
+    slug: "getting-started",
+    title: "Getting started",
+    image: "elkfefe.png",
+    excerpt: "SSsdWDWD asdasd sadasff asfdfweq fwfdwqWw",
+    date: "2022-02-10"
+},
+{
+    slug: "getting-started2",
+    title: "Getting started",
+    image: "elkfefe.png",
+    excerpt: "SSsdWDWD asdasd sadasff asfdfweq fwfdwqWw",
+    date: "2022-02-10"
+},
+{
+    slug: "getting-started3",
+    title: "Getting started",
+    image: "elkfefe.png",
+    excerpt: "SSsdWDWD asdasd sadasff asfdfweq fwfdwqWw",
+    date: "2022-02-10"
+}]
 
 function AllPoststPage() {
+    return <AllPosts posts={DUMMY_POSTS} />
 
 }
 
