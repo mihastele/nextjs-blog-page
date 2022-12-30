1d7382d6326cc7efc88c0ae6ef5f7c159d316b69
Miha Stele <stele1000@gmail.com>
2022-12-30 14:48:01 +0100

    Add dummy posts data

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,33 @@ import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 
+const DUMMY_POSTS = [{
+  slug: "getting-started",
+  title: "Getting started",
+  image: "elkfefe.png",
+  excerpt: "SSsdWDWD asdasd sadasff asfdfweq fwfdwqWw",
+  date: "2022-02-10"
+},
+{
+  slug: "getting-started2",
+  title: "Getting started",
+  image: "elkfefe.png",
+  excerpt: "SSsdWDWD asdasd sadasff asfdfweq fwfdwqWw",
+  date: "2022-02-10"
+},
+{
+  slug: "getting-started3",
+  title: "Getting started",
+  image: "elkfefe.png",
+  excerpt: "SSsdWDWD asdasd sadasff asfdfweq fwfdwqWw",
+  date: "2022-02-10"
+}]
 
 export default function Home() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   )
 }
